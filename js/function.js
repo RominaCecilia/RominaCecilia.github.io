29d11a863796dfa7d065beafcda335744f736f40
RominaCecilia <126636003+RominaCecilia@users.noreply.github.com>
2023-03-30 19:50:01 -0300

    Add files via upload

---
+++ js/function.js
@@ -0,0 +1,6 @@
+
+document.getElementById('boton').onclick = function () {
+    console.log("test");
+    document.getElementById("demo").innerHTML = "Tengo un transfondo humilde, en el cual pude aprender a adaptarme a los eventos dificiles y empatizar con terceros en estas situaciones";
+}
+
